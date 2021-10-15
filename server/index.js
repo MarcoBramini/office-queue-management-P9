0b07043643a491e25ad24613e4fd95bf1b17c596
MarcoBramini <braminim@gmail.com>
2021-10-15 10:35:56 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const { json } = require("express");
+const path = require("path");
 const cors = require("cors");
 const { getTicketsByServiceType } = require("./dao/dao");
-const app = express();
+
 const port = 3001;
 
+const app = express();
+
 app.use(cors());
+app.use(json());
+
+app.use("/", express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("/tickets/next", (req, res) => {
   getTicketsByServiceType("bills-payment")
