fff71885c1d842391c1b72d5c1854869644646cc
MarcoBramini <braminim@gmail.com>
2021-10-15 20:46:32 +0200

    Add express validator example

--- server/index.js
+++ server/index.js
@@ -1,26 +1,32 @@
 const express = require("express");
-const { json } = require("express");
 const path = require("path");
 const cors = require("cors");
 const { getTicketsByServiceType } = require("./dao/dao");
+const { param } = require("express-validator");
 
 const port = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
 app.use("/", express.static(path.resolve(__dirname, "../client/build")));
 
-app.get("/tickets/:serviceTypeId", (req, res) => {
-  const serviceTypeId = req.params.serviceTypeId;
-  getTicketsByServiceType(serviceTypeId)
-    .then((tickets) => {
-      res.send(tickets);
-    })
-    .catch((err) => console.error("error reading data from database: " + err));
-});
+app.get(
+  "/tickets/:serviceTypeId",
+  param("serviceTypeId").isString(),
+  (req, res) => {
+    const serviceTypeId = req.params.serviceTypeId;
+    getTicketsByServiceType(serviceTypeId)
+      .then((tickets) => {
+        res.send(tickets);
+      })
+      .catch((err) =>
+        console.error("error reading data from database: " + err)
+      );
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server listening at :${port}`);
