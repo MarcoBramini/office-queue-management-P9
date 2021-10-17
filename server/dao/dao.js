8414e88a656ac06789e748698435c60718ab3021
AndreaSR95 <56077426+AndreaSR95@users.noreply.github.com>
2021-10-17 22:36:37 +0200

    Server part API new ticket
    Backend api to post a new ticket

--- server/dao/dao.js
+++ server/dao/dao.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const dayjs = require('dayjs')
+
 const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(process.env.MONGO_CONN_STR);
@@ -18,3 +20,47 @@ exports.getTicketsByServiceType = async (serviceTypeId) => {
     .find({ serviceTypeId: serviceTypeId })
     .toArray();
 };
+
+exports.addNewTicket = async (ticketNumber, serviceTypeId) => {
+  let newTicket = {
+    number: ticketNumber,
+    issuedAt: dayjs().toDate(),
+    serviceTypeId: serviceTypeId,
+    status:"waiting"
+  }
+  await db
+  .collection("tickets")
+  .insertOne(newTicket);
+  return newTicket;
+}
+
+exports.getServiceType = async (serviceTypeId) => {
+  return await db
+  .collection("service-types")
+  .findOne({id: serviceTypeId});
+}
+
+//in today tickets,for a service, find the max ticket number
+exports.getServiceType_TodayMaxNumber = async (serviceTypeId) => {
+
+  //all tickets of a service type
+  let tickets_ServiceType = await db //TODO: may change this with getTicketsByServiceType function
+  .collection("tickets")
+  .find({serviceTypeId: serviceTypeId}).toArray();
+
+  //all today tickets of the service type
+  let tickets_ServiceType_day = tickets_ServiceType.filter((ticket)=>{
+    return dayjs().isSame(dayjs(ticket.issuedAt),"day") == true;
+  });
+
+  if(tickets_ServiceType_day.length==0)
+    return 0;
+
+  let ticketsNumbers = tickets_ServiceType_day.map(ticket=>ticket.number);
+  return ticketsNumbers.reduce((max, ticketNumber)=>{
+    let x = ticketNumber.substring(1);
+    if(x>max)
+      return x;
+    return max;
+  }, 0);
+}
--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const { param } = require("express-validator");
 const path = require("path");
 const cors = require("cors");
+const morgan = require('morgan');
 
-const { getTicketsByServiceType } = require("./dao/dao");
+const { getTicketsByServiceType, addNewTicket, getServiceType, getServiceType_TodayMaxNumber  } = require("./dao/dao");
+const { domainToASCII } = require("url");
 
 const port = process.env.PORT || 3001;
 
@@ -11,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.use("/", express.static(path.resolve(__dirname, "../client/build")));
 
@@ -28,6 +31,41 @@ app.get(
       );
   }
 );
+
+async function generateTicketNumber(serviceTypeId){
+  let ticketNumber;
+  //From service_type to label (service type letter)
+  await getServiceType(serviceTypeId).then((serviceType)=>{
+    ticketNumber = serviceType.ticketLabel;
+  }).catch((err)=>{
+    console.error(err);
+    ticketNumber = null;
+  })
+
+  //from service type to today max number + join strings
+  return await getServiceType_TodayMaxNumber(serviceTypeId).then(maxNumber=>{
+    maxNumber++;
+    let formattedNumber = ("0" + maxNumber).slice(-2);//to have number on 2 digits
+    ticketNumber = ticketNumber + "" + formattedNumber;
+    return ticketNumber;
+  }).catch(err=>{
+    console.error(err);
+  })
+}
+
+//Create new ticket
+app.post("/tickets", (req, res)=>{
+  let serviceType = req.body.serviceType;
+  generateTicketNumber(serviceType).then(ticketNumber=>{//if ticket number generated correctly...
+    addNewTicket(ticketNumber, serviceType).then((newTicket)=>{//create new ticket with the number
+      res.send(newTicket);//return ticket to client
+    }).catch((err)=>{
+      console.error(err);
+    });
+  }).catch(err=>{
+    console.error(err);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server listening at :${port}`);
