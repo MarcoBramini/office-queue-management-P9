a8e38b9692d39f4c51e7815f40658b99718f729e
MarcoBramini <braminim@gmail.com>
2021-10-18 19:55:24 +0200

    Update API.js

--- client/src/API.js
+++ client/src/API.js
@@ -1,56 +1,51 @@
-const BASEURL = '/api';
-const url = "http://localhost:3001/";
-
 // USER API
 
 // permette ad un utente (già registrato) di effettuare il login
 // ritorna l'utente (id, username, name) corrispondente a username/password inseriti
 async function logIn(credentials) {
-    let response = await fetch('/api/sessions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    });
-    if (response.ok) {
-      const user = await response.json();
-      return user;
-    }
-    else {
-      try {
-        const errDetail = await response.json();
-        throw errDetail.message;
-      }
-      catch (err) {
-        throw err;
-      }
+  let response = await fetch("/api/sessions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+  if (response.ok) {
+    const user = await response.json();
+    return user;
+  } else {
+    try {
+      const errDetail = await response.json();
+      throw errDetail.message;
+    } catch (err) {
+      throw err;
     }
   }
-  
-  // permette ad un utente di effettuare il logout
-  async function logOut() {
-    await fetch('/api/sessions/current', { method: 'DELETE' });
-  }
-  
-  // restituisce informazioni relative all'utente loggato (id, username, name)
-  async function getUserInfo() {
-    const response = await fetch(BASEURL + '/sessions/current');
-    const userInfo = await response.json();
-    if (response.ok) {
-      return userInfo;
-    } else {
-      throw userInfo;  // un oggetto che contiene l'errore proveniente dal server 
-    }
-  }
+}
 
-  async function callNextTicket(counterId) {
-    const response = await fetch(url + "tickets/serve/" + counterId);
-  
-    const ticket = await response.json();
-  
-    return ticket;
+// permette ad un utente di effettuare il logout
+async function logOut() {
+  await fetch("/api/sessions/current", { method: "DELETE" });
+}
+
+// restituisce informazioni relative all'utente loggato (id, username, name)
+async function getUserInfo() {
+  const response = await fetch("api/sessions/current");
+  const userInfo = await response.json();
+  if (response.ok) {
+    return userInfo;
+  } else {
+    throw userInfo; // un oggetto che contiene l'errore proveniente dal server
   }
+}
+
+async function callNextTicket(counterId) {
+  const response = await fetch("tickets/serve/" + counterId);
+
+  const ticket = await response.json();
+
+  return ticket;
+}
 
 async function postNewServiceType(newService) {
   const response = await fetch("serviceTypes", {
@@ -70,5 +65,12 @@ async function getServicesTypes() {
   return services;
 }
 
-const API = { logIn, logOut, getUserInfo, postNewServiceType, getServicesTypes, callNextTicket};
+const API = {
+  logIn,
+  logOut,
+  getUserInfo,
+  postNewServiceType,
+  getServicesTypes,
+  callNextTicket,
+};
 export default API;
