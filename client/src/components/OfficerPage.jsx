5056246393154884d3eef7d65f4ef2acafe71c48
Ahmet Kaan Ipekoren <akipekoren@gmail.com>
2021-10-18 14:12:49 +0200

    fix2

--- client/src/components/OfficerPage.jsx
+++ client/src/components/OfficerPage.jsx
@@ -23,7 +23,10 @@ export default function OfficerPage() {
           if (!ticket.error) {
             setCurrentTicket(ticket);
             console.log(ticket);
+
             setWarning("");
+            setMessage(currentTicket);
+            setMessage("");
             setMessage("You are serving Ticket number : " + ticket.number);
           } else {
             setMessage("");
