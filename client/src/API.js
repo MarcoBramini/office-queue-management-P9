1d48c1b545f388b3ecde911cebc5f61976fcc90b
AdrianaProvenzano <adriana.prove97@gmail.com>
2021-10-18 18:49:34 +0200

    fixed merge

--- client/src/API.js
+++ client/src/API.js
@@ -51,24 +51,6 @@ async function logIn(credentials) {
   
     return ticket;
   }
-  
-  async function postNewServiceType(newService) {
-    const response = await fetch(url + "serviceTypes", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ ...newService }),
-    });
-  
-    if (response.ok) return null;
-    else return { err: "post error" };
-  }
-  
-  async function getServicesTypes() {
-    const response = await fetch(url + "serviceTypes");
-    const services = await response.json();
-  
-    return services;
-  }
 
 async function postNewServiceType(newService) {
   const response = await fetch("serviceTypes", {
