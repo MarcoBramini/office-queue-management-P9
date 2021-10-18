7da1784c2363ce9b2c891f4a38ae1fbd303b0ad3
MrGreensushi <75216840+MrGreensushi@users.noreply.github.com>
2021-10-18 14:48:17 +0200

    re-inserted all the previuos tests (#8)

--- server/test/test.js
+++ server/test/test.js
@@ -28,6 +28,7 @@ after(() => {
 
 
 
+
 // Call Ticket API tests
 describe("Call ticket Apis testing:", () => {
 
@@ -167,5 +168,98 @@ describe("Call ticket Apis testing:", () => {
 
     }); 
     
+  });
+});
+
+
+// Request Tickets APIs tests
+describe("Start Tickets APIs testing:", () => {
+  beforeEach(() => mongoUnit.load(testData.ticketsCollection));
+
+  afterEach(() => mongoUnit.drop());
+
+  describe("GET /tickets/bills-payment", () => {
+    it("it should retrieve all the tickets associated to the given serviceTypeId", (done) => {
+      chai
+        .request(server)
+        .get("/tickets/bills-payment")
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res.status).to.be.equal(200);
+          expect(res.body.length).to.be.equal(2);
+          expect(res.body).to.be.an("array");
+          expect(res.body.map((t) => t.serviceTypeId)).to.include.members([
+            "bills-payment",
+          ]);
+
+          done();
+        });
+    });
+  });
+});
+
+
+// ServiceType APIs tests
+describe("Start ServiceTypes APIs testing:", () => {
+  before(() => mongoUnit.load(testData.serviceTypesCollection));
+
+  after(() => mongoUnit.drop());
+
+  const testService = {
+    id: "Boringstuff",
+    counterIDs: ["2", "4", "6"],
+    avgServingTime: 15,
+    ticketLabel: "B"
+  };
+
+  describe("POST /serviceTypes", () => {
+    it("it should write the service type to the database ", (done) => {
+      //check if Something was written on the DB
+      chai
+        .request(server)
+        .post("/serviceTypes")
+        .send(testService)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res.status).to.be.equal(200);
+          expect(res.body).to.be.an("object", res.body);
+          expect(res.body.acknowledged).to.be.equal(true);
+          done();
+        });
+    });
+
+    it("The written record should be equal", (done) =>{
+       chai
+       .request(server)
+       .get("/serviceTypes/" + testService.id)
+       .end((err, res) => {
+         expect(err).to.be.null;
+         expect(res.status).to.be.equal(200);
+         expect(res.body).to.be.an("object");
+         expect(res.body.id).to.be.equal("Boringstuff");
+         expect(res.body.avgServingTime).to.be.equal(15);
+         expect(res.body.ticketLabel).to.be.equal("B");
+         expect(res.body.counterIDs).to.have.members(["2", "4", "6"]);
+         done();
+       });
+    })
+  });
+
+  describe("GET /serviceTypes/:servicetypeId", () => {
+    it("it should retrieve the service type associated to the given serviceTypeId", (done) => {
+      chai
+        .request(server)
+        .get("/serviceTypes/bills-payment")
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res.status).to.be.equal(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body.id).to.be.equal("bills-payment");
+          expect(res.body.avgServingTime).to.be.equal(30);
+          expect(res.body.ticketLabel).to.be.equal("A");
+          expect(res.body.counterIDs).to.have.members(["1", "3", "7"]);
+          done();
+        });
+    });
   });
 });
